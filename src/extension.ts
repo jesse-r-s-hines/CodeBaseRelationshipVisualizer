cc4932a0225214fb67808867521237cf6c0785f8
Jesse Hines <jesse.r.s.hines@gmail.com>
2021-10-17 19:41:28 -0400

    Initial commit
    Scaffolding from [this tutorial](https://code.visualstudio.com/api/extension-guides/webview)

---
+++ src/extension.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('catCoding.start', () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        'catCoding',
+        'Cat Coding',
+        vscode.ViewColumn.One,
+        {}
+      );
+
+      // And set its HTML content
+      panel.webview.html = getWebviewContent();
+    })
+  );
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
