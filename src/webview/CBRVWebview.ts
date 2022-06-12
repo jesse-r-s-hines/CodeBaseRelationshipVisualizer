c728a94c7ec46bc5edd010b43513614f61d1bbef
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-06-12 18:11:59 -0400

    Refactor diagram render

--- src/webview/CBRVWebview.ts
+++ src/webview/CBRVWebview.ts
@@ -9,6 +9,19 @@ export default class CBRVWebview {
     canvas: HTMLElement
     codebase: Directory
 
+    // Settings and constants for the diagram
+
+    /** Size (width and height) of the svg viewbox (not the actual pixel size, that's dynamic) */
+    viewBoxSize = 1000
+    /** Margins of the svg diagram [top, right, bottom, left] */
+    margins = [1, 1, 1, 1]
+    /** Padding between file circles */
+    padding = 3
+    /** Directory outline stroke color */
+    stroke = "#bbb"
+    /** Directory outline stroke width */
+    strokeWidth = 1;
+
     /** Pass the selector for the canvas */
     constructor(canvas: string, codebase: Directory) {
         this.canvas = document.querySelector(canvas)!;
@@ -17,144 +30,68 @@ export default class CBRVWebview {
     }
 
     draw() {
-        const exts = new Set(d3.hierarchy(this.codebase).descendants().map(d => getExtension(d.data.name)));
-        const color = d3.scaleOrdinal(exts, d3.quantize(d3.interpolateRainbow, exts.size));
-
-        const diagram = Pack(this.codebase, {
-            value: d => d.type == FileType.File ? d.size : 0, // size of each node (file); null for internal nodes (folders)
-            label: (d) => d.data.name,
-            title: (d) => d.ancestors().reverse().map((d) => d.data.name).join("/"),
-            fill: d => color(getExtension(d.data.name)),
-            width: 1152,
-            height: 1152
-        });
-
+        const diagram = this.drawDiagram();
         this.canvas.replaceChildren(diagram);
     }
 
-}
+    drawDiagram(): SVGSVGElement {
+        const root = d3.hierarchy<AnyFile>(this.codebase, d => d.type == FileType.Directory ? d.children : undefined);
 
-// TODO: clean up types
+        // Compute size of folders
+        root.sum(d => d.type == FileType.File ? d.size : 0);
 
-type D3Val = null | string | number | boolean | ReadonlyArray<string | number>
-type D3Attr<Datum, Rtrn = D3Val> = Rtrn | ValueFn<BaseType, Datum, Rtrn>;
-type PackAttr<Rtrn = D3Val> = D3Attr<d3.HierarchyNode<AnyFile>, Rtrn>;
+        // Sort by descending size for pleasing layout
+        root.sort((a, b) => d3.descending(a.value, b.value));
 
-interface PackSettings {
-    children?: (d: AnyFile) => Iterable<AnyFile>, // if hierarchical data, given a d in data, returns its children
-    value?: (d: AnyFile) => number, // given a node d, returns a quantitative value (for area encoding; null for count)
-    sort?: (a: d3.HierarchyNode<AnyFile>, b: d3.HierarchyNode<AnyFile>) => number, // how to sort nodes prior to layout
-    label?: (d: d3.HierarchyNode<AnyFile>) => string, // given a leaf node d, returns the display name
-    title?: (d: d3.HierarchyNode<AnyFile>) => string, // given a node d, returns its hover text
-    link?: (d: d3.HierarchyNode<AnyFile>) => string, // given a node d, its link (if any)
-    linkTarget?: PackAttr<string>,
-    width?: number, // outer width, in pixels
-    height?: number, // outer height, in pixels
-    margin?: number, // shorthand for margins
-    marginTop?: number, // top margin, in pixels
-    marginRight?: number, // right margin, in pixels
-    marginBottom?: number, // bottom margin, in pixels
-    marginLeft?: number, // left margin, in pixels
-    padding?: number, // separation between circles
-    fill?: (d: d3.HierarchyNode<AnyFile>) => string, // fill for leaf circles
-    fillOpacity?: string, // fill opacity for leaf circles
-    stroke?: string, // stroke for internal circles
-    strokeWidth?: number, // stroke width for internal circles
-    strokeOpacity?: number, // stroke opacity for internal circles
-}
+        // Compute colors based on file extensions
+        const extensions = new Set(root.descendants().map(d => getExtension(d.data.name)));
+        const colorScale = d3.scaleOrdinal(extensions, d3.quantize(d3.interpolateRainbow, extensions.size));
+        const fileColor = (d: AnyFile) => d.type == FileType.Directory ? "#fff" : colorScale(getExtension(d.name));
 
-// This method is based on https://observablehq.com/@d3/pack
-// Copyright 2021 Observable, Inc., Released under the ISC license.
-function Pack(data: AnyFile, { // data is either tabular (array of objects) or hierarchy (nested objects)
-    children,
-    value,
-    sort = (a, b) => d3.descending(a.value, b.value),
-    label,
-    title,
-    link, // given a node d, its link (if any)
-    linkTarget = "_blank", // the target attribute for links, if any
-    width = 640, // outer width, in pixels
-    height = 400, // outer height, in pixels
-    margin = 1, // shorthand for margins
-    marginTop = margin, // top margin, in pixels
-    marginRight = margin, // right margin, in pixels
-    marginBottom = margin, // bottom margin, in pixels
-    marginLeft = margin, // left margin, in pixels
-    padding = 3, // separation between circles
-    fill = d => "#ddd", // fill for leaf circles
-    fillOpacity, // fill opacity for leaf circles
-    stroke = "#bbb", // stroke for internal circles
-    strokeWidth, // stroke width for internal circles
-    strokeOpacity, // stroke opacity for internal circles
-}: PackSettings): SVGSVGElement {
-    const root = d3.hierarchy(data, children);
+        // Make the circle packing diagram
+        const [marginTop, marginRight, marginBottom, marginLeft] = this.margins;
+        const packed = d3.pack<AnyFile>()
+            .size([this.viewBoxSize - marginLeft - marginRight, this.viewBoxSize - marginTop - marginBottom])
+            .padding(this.padding)(root);
+    
+        // render it to a SVG
+        const svg = d3.create("svg")
+            // use negatives to add margin since pack() starts at 0 0
+            .attr("viewBox", [-marginLeft, -marginTop, this.viewBoxSize, this.viewBoxSize])
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic;") // auto-resize // TODO: move this into HTML
+            .attr("text-anchor", "middle")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10);
+    
+        const node = svg.selectAll("g")
+            .data(packed.descendants())
+            .join("g")
+            .attr("transform", d => `translate(${d.x},${d.y})`);
+    
+        node.append("circle")
+            .attr("fill", d => fileColor(d.data))
+            .attr("fill-opacity", d => d.data.type == FileType.Directory ? 0.0 : 1.0) // directories are transparent
+            .attr("stroke", d => d.data.type == FileType.Directory ? this.stroke : "none") // only directories have an outline
+            .attr("stroke-width", d => d.data.type == FileType.Directory ? this.strokeWidth : null)
+            .attr("r", d => d.r);
+    
+        node.append("title")
+            .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
+    
+        const fileCircles = node.filter(d => d.data.type == FileType.File); 
+        // TODO: Fix name cropping
+        fileCircles.append("clipPath")
+            .attr("id", (d, i) => `clip-${i}`)
+            .append("circle")
+                .attr("r", d => d.r);
 
-    // Compute the values of internal nodes by aggregating from the leaves.
-    value == null ? root.count() : root.sum(value);
-
-    // Compute labels and titles.
-    const descendants = root.descendants();
-    const leaves = descendants.filter(d => !d.children);
-    const getIndex = (d: d3.HierarchyNode<AnyFile>) => leaves.findIndex(needle => needle === d);
-    const L = label == null ? null : leaves.map(d => label(d));
-    const T = title == null ? null : descendants.map(d => title(d));
-
-    // Sort the leaves (typically by descending value for a pleasing layout).
-    if (sort != null) root.sort(sort);
-
-    // Compute the layout. // TODO get the return result of this for types
-    d3.pack()
-        .size([width - marginLeft - marginRight, height - marginTop - marginBottom])
-        .padding(padding)(root);
-
-    const svg = d3.create("svg")
-        .attr("viewBox", [-marginLeft, -marginTop, width, height])
-        .attr("width", width)
-        .attr("height", height)
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("text-anchor", "middle");
-
-    const node = svg.selectAll("g")
-        .data(descendants)
-        .join("g")
-        .attr("xlink:href", link == null ? null : (d, i) => link(d))
-        .attr("target", link == null ? null : linkTarget)
-        .attr("transform", d => `translate(${(d as any).x},${(d as any).y})`);
-
-    node.append("circle")
-        .attr("fill", d => d.children ? "#fff" : fill(d))
-        .attr("fill-opacity", d => d.children ? null : (fillOpacity ?? null))
-        .attr("stroke", d => d.children ? stroke : null)
-        .attr("stroke-width", d => d.children ? (strokeWidth ?? null) : null)
-        .attr("stroke: number|string-opacity", d => d.children ? (strokeOpacity ?? null) : null)
-        .attr("r", d => (d as any).r);
-
-    if (T) node.append("title").text((d, i) => T[i]);
-
-    if (L) {
-        const leaf = node.filter(d => !d.children && L[getIndex(d)] != null && (d as any).r > L[getIndex(d)].length * 2.5);
-
-        leaf.append("clipPath")
-            .attr("id", d => `clip-${getIndex(d)}`)
-        .append("circle")
-            .attr("r", d => (d as any).r);
-
-        leaf.append("text")
-            .attr("clip-path", d => `url(#clip-${getIndex(d)})`)
+        fileCircles.append("text")
+            .attr("clip-path", (d, i) => `url(#clip-${i})`)
             .append("tspan")
                 .attr("x", 0)
                 .attr("y", '0.35em')
-                .text(d => L[getIndex(d)]);
+                .text(d => d.data.name);
+    
+        return svg.node()!;
     }
-
-    return svg.node()!;
 }
-
-
-
-
-
-
-
