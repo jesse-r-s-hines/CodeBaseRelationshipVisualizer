bb6fcac51756a59040529cf71c6fe0ae373f42ca
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-06-12 16:17:02 -0400

    Refactoring to extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,36 @@
 import * as vscode from 'vscode';
-import { workspace, Uri, Webview } from 'vscode';
+import { workspace, Uri, Webview, FileType } from 'vscode';
 import * as path from 'path';
-import { AnyFile, FileType } from "./util";
+import { AnyFile } from "./util";
 
 export function activate(context: vscode.ExtensionContext) {
     console.log("CodeBase Relationship Visualizer active");
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('codeBaseRelationshipVisualizer.start', async () => {
-            const folder = await getWorkspaceFileTree();
-
-            if (folder) {
-                // Create and show panel
-                const panel = vscode.window.createWebviewPanel(
-                    'codeBaseRelationshipVisualizer',
-                    'CodeBase Relationship Visualizer',
-                    vscode.ViewColumn.One,
-                    {
-                        enableScripts: true,
-                        localResourceRoots: [vscode.Uri.file(context.extensionPath)],
-                    }
-                );
-
-                panel.webview.html = getWebviewContent(context, panel.webview);
-
-                panel.webview.postMessage({ type: "set-codebase", folder: folder });
-            } else {
-                // TODO: no workspace
-            }
-        })
+        vscode.commands.registerCommand('codeBaseRelationshipVisualizer.start', async () => visualizerStart(context)),
     );
 }
 
-async function getWorkspaceFileTree(): Promise<AnyFile | undefined> {
-    if (vscode.workspace.workspaceFolders !== undefined) {
-        const base = vscode.workspace.workspaceFolders[0].uri;
-        return await getFileTree(base, FileType.Directory);
-    } else {
-        return undefined;
-    }
-}
+async function visualizerStart(context: vscode.ExtensionContext) {
+    const folder = await getWorkspaceFileTree();
 
-async function getFileTree(uri: Uri, type: FileType): Promise<AnyFile> {
-    const name = path.basename(uri.fsPath);
-    if (type == FileType.Directory) {
-        const files = await workspace.fs.readDirectory(uri);
-        const children = await Promise.all(files.map(([name, type]) => getFileTree(Uri.joinPath(uri, name), type)));
-        return { type, name, children: children };
-    } else if (type == FileType.File) {
-        return { type, name, size: (await workspace.fs.stat(uri)).size || 1 };
+    if (folder) {
+        // Create and show panel
+        const panel = vscode.window.createWebviewPanel(
+            'codeBaseRelationshipVisualizer',
+            'CodeBase Relationship Visualizer',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                localResourceRoots: [vscode.Uri.file(context.extensionPath)],
+            }
+        );
+
+        panel.webview.html = getWebviewContent(context, panel.webview);
+
+        panel.webview.postMessage({ type: "set-codebase", folder: folder });
     } else {
-        throw new Error("Other file types not supported"); // TODO handle symlinks and other special files
+        // TODO: no workspace
     }
 }
 
@@ -74,3 +54,27 @@ function getWebviewContent(context: vscode.ExtensionContext, webview: Webview) {
         </html>
     `;
 }
+
+async function getWorkspaceFileTree(): Promise<AnyFile | undefined> {
+    if (vscode.workspace.workspaceFolders !== undefined) {
+        const base = vscode.workspace.workspaceFolders[0].uri;
+        return await getFileTree(base, FileType.Directory);
+    } else {
+        return undefined;
+    }
+}
+
+
+async function getFileTree(uri: Uri, type: FileType): Promise<AnyFile> {
+    const name = path.basename(uri.fsPath);
+    if (type == FileType.Directory) {
+        const files = await workspace.fs.readDirectory(uri);
+        const children = await Promise.all(files.map(([name, type]) => getFileTree(Uri.joinPath(uri, name), type)));
+        return { type, name, children: children };
+    } else if (type == FileType.File) {
+        return { type, name, size: (await workspace.fs.stat(uri)).size || 1 };
+    } else {
+        throw new Error("Other file types not supported"); // TODO: handle symlinks and other special files
+    }
+}
+
