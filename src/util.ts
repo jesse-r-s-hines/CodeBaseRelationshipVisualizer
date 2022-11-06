a21455e4cae609804c021b681e2d741936d488fa
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-11-05 20:54:30 -0400

    Add loopIndex util

--- src/util.ts
+++ src/util.ts
@@ -32,3 +32,11 @@ export function normalizedJSONStringify(val: any) {
 
     return JSON.stringify(val, replacer)
 }
+
+/**
+ * Converts i into a number in the range [0, len) such that len + 1 = 0 and -1 = len
+ * Can be used to convert a number into a valid index in an array with circular semantics
+ */
+ export function loopIndex(i: number, len: number): number {
+    return (i < 0 ? len : 0) + i % len;
+}
