1d367120a793fd44ee155fe977c7fb709a38c94c
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-11-11 19:54:00 -0500

    Fix bugs in util.ts

--- src/util.ts
+++ src/util.ts
@@ -2,14 +2,17 @@ import { FileType, AnyFile } from "./shared";
 
 /**
  * Returns the extension from a file name excluding the ".", or "" if there is none.
- * Hidden files count as having no extension
+ * Hidden files count as having no extension.
  */
 export function getExtension(filename: string): string {
+    filename = filename.split("/").at(-1)! // remove any path
     const dotPos = filename.lastIndexOf(".");
-    return dotPos > 0 ? filename.slice(dotPos + 1) : ""; // TODO hidden files and such?
+    return dotPos > 0 ? filename.slice(dotPos + 1) : "";
 }
 
-/** Filters a tree structure */
+/**
+ * Filters a tree structure. Can't remove the root node.
+ */
 export function filterFileTree<T extends AnyFile>(root: T, condition: (node: AnyFile) => boolean): T {
     if (root.type == FileType.Directory) {
         return { ...root, children: root.children.filter(condition).map(child => filterFileTree(child, condition)) };
@@ -23,7 +26,7 @@ export function filterFileTree<T extends AnyFile>(root: T, condition: (node: Any
  */
 export function normalizedJSONStringify(val: any) {
     const replacer = (key: string, val: any) => {
-        if (typeof val == 'object' && !Array.isArray(val)) {
+        if (typeof val == 'object' && val !== null && !Array.isArray(val)) {
             return Object.keys(val).sort().reduce<Record<string, any>>((o, k) => { o[k] = val[k]; return o }, {});
         } else {
             return val;
@@ -38,7 +41,7 @@ export function normalizedJSONStringify(val: any) {
  * Can be used to convert a number into a valid index in an array with circular semantics
  */
 export function loopIndex(i: number, len: number): number {
-    return (i < 0 ? len : 0) + i % len;
+    return (i >= 0) ? i % len : len + (i + 1) % len - 1;
 }
 
 /** Makes a set of keys on type optional */
