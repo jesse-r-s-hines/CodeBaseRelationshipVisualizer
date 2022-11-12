65c34f2847fd99646bd80b2e9f27cb26d673ea97
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-11-12 14:00:31 -0500

    Add integration tests

---
+++ test/integration/extension.test.ts
@@ -0,0 +1,18 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import * as util from '../../src/util'
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+// import * as myExtension from '../../extension';
+
+suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+
+    test('Sample test', () => {
+        assert.strictEqual(util.getExtension("a.txt"), "txt");
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+});
