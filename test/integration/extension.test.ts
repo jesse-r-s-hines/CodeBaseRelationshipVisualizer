4c2e0efdb1b4d80f7b202526c953d6873c3ac11f
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-11-13 19:53:10 -0500

    Add fileHelper test

--- test/integration/extension.test.ts
+++ test/integration/extension.test.ts
@@ -1,18 +1,61 @@
-import * as assert from 'assert';
+import { expect } from 'chai';
+import { describe, test } from "mocha"
 import * as vscode from 'vscode';
+import { Uri } from 'vscode'
 
-import * as util from '../../src/util'
+import { FileType } from '../../src/shared'
+import * as fileHelper from '../../src/fileHelper'
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as myExtension from '../../extension';
+// I can't find a built-in way to get workspaceFolder. __dirname is .../CBRV/dist/test/test/integration
+const workspaceFolder = Uri.file(__dirname.split("/").slice(0, -4).join("/"))
+const samples = Uri.joinPath(workspaceFolder, `/test/sample-codebases`);
 
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+describe('Test fileHelper', () => {
+    test('getWorkspaceFileTree', async () => {
+        // Opening files doesn't work reliably, so we are just opening sample-codebases in the launch script.
+        // await vscode.commands.executeCommand("vscode.openFolder", Uri.joinPath(samples, "minimal"));
 
-    test('Sample test', () => {
-        assert.strictEqual(util.getExtension("a.txt"), "txt");
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+        const tree = await fileHelper.getWorkspaceFileTree();
+        expect(tree?.name).to.eql("sample-codebases")
+        // we'll test the content in getFileTree directly
+    });
+
+    test('getFileTree', async () => {
+        const tree = await fileHelper.getFileTree(Uri.joinPath(samples, "minimal"));
+        expect(tree).to.eql({
+            "name": "minimal",
+            "type": FileType.Directory,
+            "children": [
+                {
+                    "name": "A",
+                    "type": FileType.Directory,
+                    "children": [
+                        {"name": "E.txt", "size": 1828, "type": FileType.File},
+                        {"name": "F.txt", "size": 630, "type": FileType.File},
+                        {"name": "G.md", "size": 124, "type": FileType.File},
+                    ],
+                },
+                {"name": "C.txt", "size": 1117, "type": FileType.File},
+                {"name": "D.md", "size": 841, "type": FileType.File},
+                {"name": "Supercalifragilisticexpialidocious.py", "size": 44, "type": FileType.File},
+                {
+                    "name": "deoxyribonucleicAcid",
+                    "type": FileType.Directory,
+                    "children": [
+                        {"name": "I", "size": 1, "type": FileType.File},
+                    ],
+                }
+            ]
+        });
+    });
+
+    test('getPathSet', async () => {
+        const tree = await fileHelper.getPathSet(Uri.joinPath(samples, "minimal"));
+        expect(tree).to.eql(new Set([
+            "A/E.txt", "A/F.txt", "A/G.md",
+            "C.txt", "D.md",
+            "Supercalifragilisticexpialidocious.py",
+            "deoxyribonucleicAcid/I",
+        ]));
     });
 });
