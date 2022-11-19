6919d1dce2c8fb6602448cd59e1905aaaa56f3e3
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-11-19 16:27:11 -0500

    More test improvements

--- test/integration/fsHelper.test.ts
+++ test/integration/fsHelper.test.ts
@@ -2,14 +2,21 @@ import { expect } from 'chai';
 import { describe, test } from "mocha"
 import * as vscode from 'vscode';
 import { Uri } from 'vscode'
+import * as fs from 'fs'
 
 import { FileType } from '../../src/shared'
 import * as fileHelper from '../../src/fileHelper'
 
 // I can't find a built-in way to get workspaceFolder. __dirname is .../CBRV/dist/test/test/integration
 const workspaceFolder = Uri.file(__dirname.split("/").slice(0, -4).join("/"))
-const samples = Uri.joinPath(workspaceFolder, `/test/sample-codebases`);
-const minimal = {
+const samples = Uri.joinPath(workspaceFolder, '/test/sample-codebases');
+const minimal = Uri.joinPath(samples, 'minimal')
+const empty = Uri.joinPath(samples, 'empty')
+if (!fs.existsSync(empty.fsPath)) {
+    fs.mkdirSync(empty.fsPath) // git can't track an empty dir
+}
+
+const minimal_contents = {
     "name": "minimal",
     "type": FileType.Directory,
     "children": [
@@ -35,10 +42,12 @@ const minimal = {
     ]
 }
 
+
+
 describe('Test fileHelper', () => {
     test('getWorkspaceFileTree', async () => {
         // Opening files doesn't work reliably, so we are just opening sample-codebases in the launch script.
-        // await vscode.commands.executeCommand("vscode.openFolder", Uri.joinPath(samples, "minimal"));
+        // await vscode.commands.executeCommand("vscode.openFolder", minimal);
 
         const tree = await fileHelper.getWorkspaceFileTree();
         expect(tree?.name).to.eql("sample-codebases")
@@ -46,19 +55,49 @@ describe('Test fileHelper', () => {
     });
 
     test('getFileTree', async () => {
-        const tree = await fileHelper.getFileTree(Uri.joinPath(samples, "minimal"));
-        expect(tree).to.eql(minimal);
+        let tree = await fileHelper.getFileTree(minimal);
+        expect(tree).to.eql(minimal_contents);
+
+        tree = await fileHelper.getFileTree(Uri.joinPath(samples, "empty"));
+        expect(tree).to.eql({type: FileType.Directory, name: "empty", children: []});
     });
 
     test('listToFileTree', async () => {
-        const includePattern = new vscode.RelativePattern(Uri.joinPath(samples, "minimal"), '**/*')
-        const fileList = await vscode.workspace.findFiles(includePattern)
-        const tree = await fileHelper.listToFileTree(Uri.joinPath(samples, "minimal"), fileList)
-        expect(tree).to.eql(minimal);
+        const includePattern = new vscode.RelativePattern(minimal, '**/*')
+        let fileList = await vscode.workspace.findFiles(includePattern)
+        let tree = await fileHelper.listToFileTree(minimal, fileList)
+        expect(tree).to.eql(minimal_contents);
+
+        tree = await fileHelper.listToFileTree(empty, [])
+        expect(tree).to.eql({type: FileType.Directory, name: "empty", children: []});
+
+        fileList = [
+            Uri.joinPath(minimal, 'A')
+        ]
+        tree = await fileHelper.listToFileTree(minimal, fileList)
+        expect(tree).to.eql({
+            type: FileType.Directory,
+            name: "minimal",
+            children: [{type: FileType.Directory, name: "A", children: []}],
+        });
+
+        fileList = [
+            Uri.joinPath(minimal, 'A/E.txt')
+        ]
+        tree = await fileHelper.listToFileTree(minimal, fileList)
+        expect(tree).to.eql({
+            type: FileType.Directory,
+            name: "minimal",
+            children: [{
+                type: FileType.Directory,
+                name: "A",
+                children: [{type: FileType.File, name: "E.txt", size: 1828}]
+            }],
+        });
     });
 
     test('getPathSet', async () => {
-        const tree = await fileHelper.getPathSet(Uri.joinPath(samples, "minimal"));
+        const tree = await fileHelper.getPathSet(minimal);
         expect(tree).to.eql(new Set([
             "A/E.txt", "A/F.txt", "A/G.md",
             "C.txt", "D.md",
