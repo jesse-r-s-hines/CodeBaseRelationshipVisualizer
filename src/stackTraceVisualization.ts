cf37c670a7c2fdd14227d679ed01ef29b030e7f0
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-12-04 21:20:15 -0500

    Mulithreads working

--- src/stackTraceVisualization.ts
+++ src/stackTraceVisualization.ts
@@ -1,11 +1,14 @@
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
 import { API, Visualization, VisualizationSettings, Connection } from "./api";
-import {DebugAdapterTracker, DebugSession} from 'vscode';
-import {DebugProtocol} from 'vscode-debugprotocol';
+import { DebugAdapterTracker, DebugSession } from 'vscode';
+import { DebugProtocol } from 'vscode-debugprotocol';
 import * as path from "path";
 import _ from "lodash";
 
+const colorScheme = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+];
 
 export async function activate(context: vscode.ExtensionContext) {
     const cbrvAPI = new API(context);
@@ -26,7 +29,8 @@ async function createStackTraceVisualization(cbrvAPI: API): Promise<Visualizatio
         directed: true,
         connectionDefaults: {
             tooltip: (conn) => {
-                const tooltips = conn.connections.map(c => `-> "${c.toName}" "${c.to!.file}:${c.to!.line}"`);
+                const tooltips = conn.connections
+                    .map(c => `to Thread ${c.threadId} "${c.toName}" "${c.to!.file}:${c.to!.line}"`);
                 return _(tooltips)
                     .countBy()
                     .toPairs()
@@ -40,42 +44,62 @@ async function createStackTraceVisualization(cbrvAPI: API): Promise<Visualizatio
             line: "ignore",
             direction: "same",
             width: { rule: "add", max: 4 },
+            threadId: "same",
         },
     };
 
     return await cbrvAPI.create(settings);
 }
 
+
+type Frame = { file: string, line: number, name: string }
+
 class StackTraceVisualization implements vscode.DebugAdapterTrackerFactory {
     visualization?: Visualization;
-    stackTrace?: {file: string, line: number, name: string}[];
+    stackTraces: Map<number, Frame[]> = new Map();
 
     createDebugAdapterTracker(session: DebugSession): DebugAdapterTracker {
         return {
             onDidSendMessage: async (msg: DebugProtocol.ProtocolMessage) => {
                 if (msg.type == "event" && (msg as DebugProtocol.Event).event == "stopped") {
                     const stoppedMsg = msg as DebugProtocol.StoppedEvent;
-                    const threadId = stoppedMsg.body.threadId;
+                    const threadId = stoppedMsg.body.threadId!;
 
-                    // TODO VSCode docs imply I need to do "paging" of the stackTrace? I can't get python debugging
-                    // to require paging, may be implementation dependent.
-                    const stackTrace = await session.customRequest("stackTrace", {
-                        threadId: threadId,
-                    }) as DebugProtocol.StackTraceResponse['body'];
+                    this.stackTraces.set(threadId, []); // doesn't matter if we overwrite it, we're re-fetching anyways
 
-                    this.stackTrace = [...stackTrace.stackFrames]
-                        .reverse()
-                        .map((frame) => ({
-                            file: frame.source!.path!,
-                            line: frame.line,
-                            name: frame.name,
-                        }));
+                    const pairs = await Promise.all(
+                        [...this.stackTraces.keys()]
+                            .map<Promise<[number, DebugProtocol.StackTraceResponse['body']]>>(async threadId => {
+                                const stackTrace = await session.customRequest("stackTrace", {
+                                    threadId: +threadId,
+                                });
+                                return [+threadId, stackTrace];
+                            })
+                    );
+
+                    console.log({ pairs });
+
+                    this.stackTraces = new Map(
+                        pairs
+                            .filter(([threadId, frame]) => frame.stackFrames.length > 0) // filter finished threads
+                            .map(([threadId, frame]) => {
+                                const simpleFrames = [...frame.stackFrames]
+                                    .reverse()
+                                    .map((frame) => ({
+                                        file: frame.source!.path!,
+                                        line: frame.line,
+                                        name: frame.name,
+                                        threadId: threadId,
+                                    }));
+                                return [threadId, simpleFrames];
+                            })
+                    );
 
                     this.updateVisualization();
                 }
             },
             onWillStopSession: async () => {
-                this.stackTrace = undefined;
+                this.stackTraces = new Map();
                 this.updateVisualization();
             }
         };
@@ -83,19 +107,28 @@ class StackTraceVisualization implements vscode.DebugAdapterTrackerFactory {
 
     updateVisualization() {
         this.visualization?.update(async visState => {
-            visState.connections = (this.stackTrace ?? [])
-                .filter(frame => visState.files.some(uri => frame.file == uri.fsPath))
-                .map((frame, i, arr) => ({
-                    from: (i == 0) ? undefined : {
-                        file: path.relative(visState.codebase.fsPath, arr[i - 1].file),
-                        line: arr[i - 1].line,
-                    },
-                    to: {
-                        file: path.relative(visState.codebase.fsPath, frame.file),
-                        line: frame.line,
-                    },
-                    toName: frame.name,
-                }));
+            visState.connections = [...this.stackTraces.entries()]
+                .flatMap(([threadId, frames], threadIndex) =>
+                    frames
+                        .filter(frame => visState.files.some(uri => frame.file == uri.fsPath))
+                        .map((frame, i, arr) => ({
+                            from: (i == 0) ? undefined : {
+                                file: path.relative(visState.codebase.fsPath, arr[i - 1].file),
+                                line: arr[i - 1].line,
+                            },
+                            to: {
+                                file: path.relative(visState.codebase.fsPath, frame.file),
+                                line: frame.line,
+                            },
+                            toName: frame.name,
+                            threadId: threadId,
+                            color: colorScheme[threadIndex % this.stackTraces.size],
+                        }))
+                );
         });
     }
 }
+
+
+// tooltip not updating whent connectiosn change
+// error on update if merge rules are falses
--- test/sample-codebases/python/a.py
+++ test/sample-codebases/python/a.py
@@ -1,4 +1,25 @@
 import b.c
+import threading
 
-print(b.c.fact(6))
-print(b.c.recursive(6))
+def thread1():
+    for i in range(10):
+        print(i)
+
+def thread2():
+    for i in range(10):
+        print(i)
+
+if __name__ == "__main__":
+    print(b.c.fact(6))
+    print(b.c.recursive(6))
+
+    t1 = threading.Thread(target=thread1)
+    t2 = threading.Thread(target=thread2)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print("Done!")
