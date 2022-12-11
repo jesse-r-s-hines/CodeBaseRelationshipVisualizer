81d63bac047a00995269fc6da28d339da54900fb
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-12-10 19:59:11 -0500

    More dependency tests

--- test/integration/dependencies.test.ts
+++ test/integration/dependencies.test.ts
@@ -7,43 +7,47 @@ import dedent from "dedent-js";
 import { getDependencyGraph } from '../../src/visualizations/pythonDependencyVisualization';
 import { writeFileTree } from "./integrationHelpers";
 
-async function testGetDependencyGraph(dir: Uri) {
-    const files = await workspace.findFiles(new RelativePattern(dir, '**/*'));
-    const x = (await getDependencyGraph(dir, files));
-    return (await getDependencyGraph(dir, files)).map(c => ({
+async function testGetDependencyGraph(dir: Uri, files?: string[]) {
+    let fileUris: Uri[];
+    if (files) {
+        fileUris = files.map(f => Uri.file(path.resolve(dir.fsPath, f)));
+    } else {
+        fileUris = await workspace.findFiles(new RelativePattern(dir, '**/*'));
+    }
+    return (await getDependencyGraph(dir, fileUris)).map(c => ({
         from: path.relative(dir.fsPath, (c.from as any).fsPath),
         to: path.relative(dir.fsPath, (c.to as any).fsPath),
     }));
 }
 
 describe("Test getDependencyGraph", () => {
-    // it('test basic', async () => {
-    //     let dir = await writeFileTree({
-    //         'mypkg/__init__.py': "",
-    //         'mypkg/a.py': dedent`
-    //             from pathlib import Path
-    //             import threading
-    //             import b.c
-    //         `,
-    //         'mypkg/b/__init__.py': "",
-    //         'mypkg/b/c.py': dedent`
-    //             from . import d
-    //         `,
-    //         'mypkg/b/d.py': dedent`
-    //             from . import c
-    //         `,
-    //     });
-    //     expect(await testGetDependencyGraph(Uri.joinPath(dir, 'mypkg'))).to.eql([
-    //         { from: "b/c.py", to: "__init__.py" },
-    //         { from: "b/c.py", to: "b/__init__.py" },
-    //         { from: "b/c.py", to: "b/d.py" },
-    //         { from: "b/d.py", to: "__init__.py" },
-    //         { from: "b/d.py", to: "b/__init__.py" },
-    //         { from: "b/d.py", to: "b/c.py" },
-    //         { from: "a.py", to: "b/__init__.py" },
-    //         { from: "a.py", to: "b/c.py" },
-    //     ]);
-    // });
+    it('test basic', async () => {
+        const dir = await writeFileTree({
+            'mypkg/__init__.py': "",
+            'mypkg/a.py': dedent`
+                from pathlib import Path
+                import threading
+                import b.c
+            `,
+            'mypkg/b/__init__.py': "",
+            'mypkg/b/c.py': dedent`
+                from . import d
+            `,
+            'mypkg/b/d.py': dedent`
+                from . import c
+            `,
+        });
+        expect(await testGetDependencyGraph(Uri.joinPath(dir, 'mypkg'))).to.eql([
+            { from: "b/c.py", to: "__init__.py" },
+            { from: "b/c.py", to: "b/__init__.py" },
+            { from: "b/c.py", to: "b/d.py" },
+            { from: "b/d.py", to: "__init__.py" },
+            { from: "b/d.py", to: "b/__init__.py" },
+            { from: "b/d.py", to: "b/c.py" },
+            { from: "a.py", to: "b/__init__.py" },
+            { from: "a.py", to: "b/c.py" },
+        ]);
+    });
 
     it('test empty', async () => {
         const dir = await writeFileTree({
@@ -79,6 +83,34 @@ describe("Test getDependencyGraph", () => {
         expect(await testGetDependencyGraph(Uri.joinPath(dir, 'mypkg'))).to.eql([
             {from: "a.py", to: "b/__init__.py"},
             {from: "a.py", to: "b/c.py"},
+        ]);
+    });
+
+    it('test excluded', async () => {
+        const dir = await writeFileTree({
+            'mypkg/__init__.py': "",
+            'mypkg/a.py': dedent`
+                import b
+            `,
+            'mypkg/b.py': dedent`
+                import c
+            `,
+            'mypkg/c.py': dedent`
+                import d
+            `,
+            'mypkg/d.py': dedent`
+                print('hello world')
+            `,
+        });
+        const actual = await testGetDependencyGraph(Uri.joinPath(dir, 'mypkg'), [
+            '__init__.py',
+            'a.py',
+            'c.py',
+            'd.py',
+        ]);
+
+        expect(actual).to.eql([
+            { from: "c.py", to: "d.py" },
         ]);
     });
 });
