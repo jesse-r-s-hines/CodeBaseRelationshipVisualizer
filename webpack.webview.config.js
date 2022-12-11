d3eda0f3ceb8da08ebfcf27950c4f724717db46c
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-12-11 16:56:20 -0500

    Rename webpack config

---
+++ webpack.webview.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = (env) => ({
+  entry: {
+    webview: "./src/webview/index.ts"
+  },
+  mode: (env.prod) ? "production" : "development",
+  devtool: (env.prod) ? 'source-map' : 'inline-source-map', // inline-source-map makes debugging work better.
+  optimization: {
+    minimize: env.prod ? true : false // Debugger has trouble if you minify, even with the source map.
+  },
+  output: {
+    path: path.resolve(__dirname, "dist", "webview"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "src/webview/tsconfig.json",
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  }
+});
