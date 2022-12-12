bd479e0b8ff80a69743b2a66893339d54e6dc89d
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-12-11 20:52:47 -0500

    Cross platform fixes to dependency visualization

--- src/visualizations/pythonDependencyVisualization.ts
+++ src/visualizations/pythonDependencyVisualization.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import { Uri, workspace, FileType } from 'vscode';
 import * as path from 'path';
-import * as child_process from 'child_process';
 import * as child_process_promise from 'child-process-promise';
 import _ from 'lodash';
 
@@ -10,7 +9,7 @@ import { API, Visualization, VisualizationSettings, Connection } from "../api";
 export async function activate(context: vscode.ExtensionContext) {
     const cbrvAPI = new API(context);
 
-    installPyDepsIfNeeded();
+    await installPyDepsIfNeeded();
 
     context.subscriptions.push(
         vscode.commands.registerCommand('pythonDependencyVisualization.start', async () => {
@@ -19,35 +18,34 @@ export async function activate(context: vscode.ExtensionContext) {
     );
 }
 
-function installPyDepsIfNeeded() {
+async function installPyDepsIfNeeded() {
     const pythonPath = vscode.workspace.getConfiguration('python').get<string>('defaultInterpreterPath', 'python');
 
-    child_process.exec(`'${pythonPath}' -m pydeps --version`, (error) => {
-        if (error) {
-            vscode.window.withProgress({
-                location: vscode.ProgressLocation.Notification,
-                title: "Installing pydeps...",
-                cancellable: true
-            }, (progress, token) => {
-                return new Promise((resolve, reject) => {
-                    const proc = child_process.exec(`'${pythonPath}' -m pip install pydeps`, (error) => {
-                        if (error) {
-                            const message = `Failed to install pydeps:\n ${error.message}`;
-                            vscode.window.showErrorMessage(message);
-                            reject(new Error(message));
-                        } else {
-                            vscode.window.showInformationMessage(`Installed pydeps`);
-                            resolve(undefined);
-                        }
-                    });
+    let installed = false;
+    try {
+        await child_process_promise.spawn(pythonPath, ['-m', 'pydeps', '--version']);
+        installed = true;
+    } catch {
+        installed = false;
+    }
 
-                    token.onCancellationRequested(() => {
-                        proc.kill();
-                    });
-                });
-            });
-        }
-    });
+    if (!installed) {
+        await vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Installing pydeps...",
+            cancellable: false
+        }, async (progress, token) => {
+            try {
+                await child_process_promise.spawn(pythonPath, ['-m', 'pip', 'install', 'pydeps'],
+                    { capture: ['stdout', 'stderr'] });
+                vscode.window.showInformationMessage(`Installed pydeps`);
+            } catch (e: any) {
+                const message = `Failed to install pydeps:\n ${e.stderr}`;
+                vscode.window.showErrorMessage(message);
+                throw new Error(message);
+            }
+        });
+    }
 }
 
 async function createPythonDependencyVisualization(cbrvAPI: API): Promise<Visualization> {
